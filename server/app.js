7d9b858e37687c3631c3ebd14bea746fda4a7efa
Dennis Fleischmann <dennis.fleischmann@hotmail.com>
2019-06-03 23:05:17 +0200

    Finished AG-Grid Virtual Scrolling Server Side Row Model

---
+++ server/app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var path = require('path');
+
+
+let database;
+
+fs.readFile(__dirname + '/data.json', function(err, data) {
+  //console.log('data', data);
+
+  var idSequence = 0;
+
+  data.forEach(function(item) {
+    item.id = idSequence++;
+  });
+
+  database = JSON.parse(data);
+
+  app.set('port', process.env.PORT || 3000);
+  app.get('/data', function(request, res) {
+
+    var startRow = request.param('startRow');
+    var endRow = request.param('endRow');
+
+    // take a slice of the total rows
+    var rowsThisPage = database.slice(startRow, endRow);
+
+    var lastRow = database.length <= endRow ? database.length : -1;
+
+    res.setHeader('Content-Type', 'application/json');
+
+    res.send({
+      success: true,
+      rows: rowsThisPage,
+      lastRow: lastRow
+    })
+
+  });
+
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'));
+  });
+
+  app.get('/main.js', function(req, res) {
+    res.sendFile(path.join(__dirname, '../main.js'));
+  });
+
+  app.listen(app.get('port'), function() {
+    console.log('http Express server(worker) listening on port ' + app.get('port'));
+  });
+  if (err) {
+    throw err;
+  }
+});
